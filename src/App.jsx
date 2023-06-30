9400a24b5b20eb931bbd87510c3733f5aea6050d
Victor Ek <victor.ek@antura.se>
2023-06-30 10:49:40 +0200

    Bump plinkosort

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,33 @@ import { Physics, useBox, useSphere, useCylinder } from "@react-three/cannon";
 import { OrbitControls, PerspectiveCamera, Html } from "@react-three/drei";
 
 // add names here that should compete against eachother
-const names = [];
+const names = ["Mikl", "Victor", "aioli", "NIls", "MOa", "Alipanter", "Emma"];
+
+/**
+ * resultat
+ * 1st: aioli
+ * 2nd: NIls
+ * 3:Rd: MOa
+ * 4fthth: Victor
+ * 5Ltjht: Alipanter
+ * 6: Mkl
+ * 7: Emma
+ */
 const pegs = [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12];
 const rows = [
   1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
 ];
 
 function Peg(props) {
-  const [ref] = useCylinder(() => ({ ...props }));
+  const [ref] = useCylinder(() => ({
+    ...props,
+    collisionResponse: 0.1,
+  }));
+
   return (
     <mesh ref={ref} castShadow receiveShadow>
       <cylinderBufferGeometry {...props} castShadow receiveShadow />
-      <meshNormalMaterial />
+      <meshBasicMaterial />
     </mesh>
   );
 }
@@ -68,19 +83,28 @@ function Wall(props) {
 
 const ranomizedNames = shuffle(names);
 
-function SpawnPlayers() {
-  const offsetX = 40;
-  return ranomizedNames.map((player, i) => (
+const SpawnPlayers = React.memo(({ players }) => {
+  const offsetX = players.length * 5;
+  return shuffle(players).map((player, i) => (
     <Sphere
       name={player}
       args={[2, 32]}
       position={[i * 10 - offsetX, 200, 0]}
     />
   ));
-}
+});
 
 function App() {
   const [winner, setWinner] = useState("");
+  const [gravity, setGravity] = useState(0);
+  const [newPlayer, setNewPlayer] = useState("");
+  const [players, setPlayers] = useState([]);
+
+  const handleReset = () => {
+    setPlayers((prev) => prev.filter((player) => player !== winner));
+    setWinner("");
+  };
+
   return (
     <Canvas shadows>
       <PerspectiveCamera
@@ -98,16 +122,39 @@ function App() {
         </div>
       </Html>
 
-      <Physics gravity={[0, -10, 0]}>
+      <Html position={[40, 250, 0]}>
+        <input onChange={(e) => setNewPlayer(e.target.value)} type="text" />
+        <button
+          type="button"
+          onClick={() => setPlayers((prev) => [...prev, newPlayer])}
+        >
+          Add player
+        </button>
+        <button type="button" onClick={handleReset}>
+          Reset
+        </button>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          defaultValue="0"
+          onChange={(e) => setGravity(-e.target.value)}
+        />
+      </Html>
+
+      <Physics gravity={[0, gravity, 0]}>
         <GeneratePegs />
-        <SpawnPlayers />
+        <SpawnPlayers players={players} />
         <Floor
           args={[130, 5, 100]}
           position={[0, -50, 0]}
           onCollide={(e) =>
-            setWinner((prev) => (prev === "" ? e.body.name : prev))
+            setWinner((prev) =>
+              prev.includes(e.body.name) ? prev : [...prev, e.body.name]
+            )
           }
         />
+        {/* TODO: add invisible walls so that no one cheats! */}
         {/* Right side walls */}
         <Wall
           position={[95, 0, 0]}
@@ -207,12 +254,15 @@ function GeneratePegs() {
             {pegs.map((peg, x) => {
               const offsetX = y % 2 === 0 ? 2.5 : -2.5;
               const offsetXtra = (pegs.length * 9) / 2;
+              const thiccnessIndex = Math.floor(
+                Math.random() * thiccnessess.length - 1
+              );
               return (
                 <Peg
                   key={"peg" + x}
                   args={[
-                    ranomizedThiccnessess[x + (y % thiccnessess.length)],
-                    ranomizedThiccnessess[x + (y % thiccnessess.length)],
+                    thiccnessess[thiccnessIndex],
+                    ranomizedThiccnessess[thiccnessIndex],
                     10,
                     42,
                   ]}
